82a5a3081aec7a151b0689bd0c6c430884296f86
Ntshuxeko Mushwana <ntshuxekomushwana7@gmail.com>
2025-09-06 18:16:16 +0200

    Complete wenesite but in need of improvements

---
+++ lightbox.js
@@ -0,0 +1,19 @@
+const images = document.querySelectorAll('img');
+const lightbox = document.getElementById('lightbox'); 
+
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active');
+    const img = document.createElement('img');
+    img.src = image.src;
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(img);
+  });
+});
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+});
